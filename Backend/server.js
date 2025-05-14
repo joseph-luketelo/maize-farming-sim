7c305cd71e7ba1c1efd696e036600898f0285db3
joseph luketelo <joseph.luketelo@gmail.com>
2025-05-14 12:29:31 +0300

    fixed cors error

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@
 const db_url = `mysql://root:pDEbQwPBxPtjiFLpuEVTeRrjVmWMjkwM@mysql.railway.internal:3306/railway`
 
 const express = require(`express`)
-const mysql = require(`mysql`)
+const mysql = require(`mysql2`)
 const cors = require(`cors`)
 
 const app = express()
