549a199fbd542d605dc2cb77a2935ed3f57631ca
joseph luketelo <joseph.luketelo@gmail.com>
2025-05-14 13:32:30 +0300

    fixed cors error

--- Backend/server.js
+++ Backend/server.js
@@ -28,9 +28,6 @@ app.post('/login', (req, res) => {
         console.log("END3")
         console.log("START4")
         console.log(data)
-        console.log(data.length)
-        console.log(res)
-        console.log(res.json)
         console.log("END4")
         if(data.length > 0){
             console.log(data)
